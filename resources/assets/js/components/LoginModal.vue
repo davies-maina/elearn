60ced981597a3bcee740fe97f193c0412d922047
Davies Maina <daviesmaina@outlook.com>
2020-01-23 13:13:34 +0300

    laravel 5.6 ins with custom html  & login form

---
+++ resources/assets/js/components/LoginModal.vue
@@ -0,0 +1,167 @@
+<template>
+    <div class="container">
+        <div id="loginModal" class="modal fade">
+	<div class="modal-dialog modal-login">
+		<div class="modal-content">
+			<div class="modal-header">				
+				<h4 class="modal-title">Sign In</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			</div>
+			<div class="modal-body">
+				<form @submit.prevent="logIn">
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+							<input type="email" class="form-control" v-model="email" placeholder="Email" required="required">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+							<input type="text" class="form-control" v-model="password" placeholder="Password" required="required">
+						</div>
+					</div>
+                    
+                    <div class="checkbox">
+    <label><input type="checkbox" v-model="remember"> Remember me</label>
+  
+                    </div>
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary btn-block btn-lg">Sign In</button>
+					</div>
+					<p class="hint-text"><a href="#">Forgot Password?</a></p>
+				</form>
+			</div>
+			<div class="modal-footer">Don't have an account? <a href="#">Create one</a></div>
+		</div>
+	</div>
+</div>     
+    </div>
+</template>
+
+<script>
+export default {
+    name:'LoginModal',
+    data() {
+        return {
+            email:'',
+            password:'',
+            remember:''
+        }
+    },
+
+    methods: {
+        logIn(){
+
+
+        }
+    },
+}
+</script>
+
+<style scoped>
+.modal-login {
+		width: 350px;
+	}
+	.modal-login .modal-content {
+		padding: 20px;
+		border-radius: 5px;
+		border: none;
+	}
+	.modal-login .modal-header {
+		border-bottom: none;
+        position: relative;
+		justify-content: center;
+	}
+	.modal-login .close {
+        position: absolute;
+		top: -10px;
+		right: -10px;
+	}
+	.modal-login h4 {
+		color: #636363;
+		text-align: center;
+		font-size: 26px;
+		margin-top: 0;
+	}
+	.modal-login .modal-content {
+		color: #999;
+		border-radius: 1px;
+    	margin-bottom: 15px;
+        background: #fff;
+		border: 1px solid #f3f3f3;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        padding: 25px;
+    }
+	.modal-login .form-group {
+		margin-bottom: 20px;
+	}
+	.modal-login label {
+		font-weight: normal;
+		font-size: 13px;
+	}
+	.modal-login .form-control {
+		min-height: 38px;
+		padding-left: 5px;
+		box-shadow: none !important;
+		border-width: 0 0 1px 0;
+		border-radius: 0;
+	}
+	.modal-login .form-control:focus {
+		border-color: #ccc;
+	}
+	.modal-login .input-group-addon {
+		max-width: 42px;
+		text-align: center;
+		background: none;
+		border-width: 0 0 1px 0;
+		padding-left: 5px;
+		border-radius: 0;
+	}
+    .modal-login .btn {        
+        font-size: 16px;
+        font-weight: bold;
+		background: #19aa8d;
+        border-radius: 3px;
+		border: none;
+		min-width: 140px;
+        outline: none !important;
+    }
+	.modal-login .btn:hover, .modal-login .btn:focus {
+		background: #179b81;
+	}
+	.modal-login .hint-text {
+		text-align: center;
+		padding-top: 5px;
+		font-size: 13px;
+	}
+	.modal-login .modal-footer {
+		color: #999;
+		border-color: #dee4e7;
+		text-align: center;
+		margin: 0 -25px -25px;
+		font-size: 13px;
+		justify-content: center;
+	}
+	.modal-login a {
+		color: #fff;
+		text-decoration: underline;
+	}
+	.modal-login a:hover {
+		text-decoration: none;
+	}
+	.modal-login a {
+		color: #19aa8d;
+		text-decoration: none;
+	}	
+	.modal-login a:hover {
+		text-decoration: underline;
+	}
+	.modal-login .fa {
+		font-size: 21px;
+	}
+	.trigger-btn {
+		display: inline-block;
+		margin: 100px auto;
+	}
+</style>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('header')
+
+<section id="home-slider">
+        <div class="container">
+            <div class="row">
+                <div class="main-slider">
+                    <div class="slide-text">
+                        <h1>We Are Creative Nerds</h1>
+                        <p>Boudin doner frankfurter pig. Cow shank bresaola pork loin tri-tip tongue venison pork belly meatloaf short loin landjaeger biltong beef ribs shankle chicken andouille.</p>
+                        <a href="#loginModal" class="btn btn-common" data-toggle="modal">SIGN UP</a>
+                    </div>
+                    <img src="images/home/slider/hill.png" class="slider-hill" alt="slider image">
+                    <img src="images/home/slider/house.png" class="slider-house" alt="slider image">
+                    <img src="images/home/slider/sun.png" class="slider-sun" alt="slider image">
+                    <img src="images/home/slider/birds1.png" class="slider-birds1" alt="slider image">
+                    <img src="images/home/slider/birds2.png" class="slider-birds2" alt="slider image">
+                </div>
+            </div>
+        </div>
+        <div class="preloader"><i class="fa fa-sun-o fa-spin"></i></div>
+    </section>
+    <!--/#home-slider-->
+    @endsection
+
+    @section('content')
+        <div class="row">
+                <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="300ms">
+                    <div class="single-service">
+                        <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="300ms">
+                            <img src="images/home/icon1.png" alt="">
+                        </div>
+                        <h2>Incredibly Responsive</h2>
+                        <p>Ground round tenderloin flank shank ribeye. Hamkevin meatball swine. Cow shankle beef sirloin chicken ground round.</p>
+                    </div>
+                </div>
+                <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
+                    <div class="single-service">
+                        <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="600ms">
+                            <img src="images/home/icon2.png" alt="">
+                        </div>
+                        <h2>Superior Typography</h2>
+                        <p>Hamburger ribeye drumstick turkey, strip steak sausage ground round shank pastrami beef brisket pancetta venison.</p>
+                    </div>
+                </div>
+                <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="900ms">
+                    <div class="single-service">
+                        <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="900ms">
+                            <img src="images/home/icon3.png" alt="">
+                        </div>
+                        <h2>Swift Page Builder</h2>
+                        <p>Venison tongue, salami corned beef ball tip meatloaf bacon. Fatback pork belly bresaola tenderloin bone pork kevin shankle.</p>
+                    </div>
+                </div>
+            </div>
+    <!--/#services-->
+    @endsection
