2a92ac01f49eaf18dcc966877727b4806b51af76
Davies Maina <daviesmaina@outlook.com>
2020-01-23 16:58:46 +0300

    tests for wrong login info (for windows run 'php vendor/phpunit/phpunit/phpunit')

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_login_wrong_credentials_text()
+    {
+        /* $this->assertTrue(true); */
+        $user = factory(User::class)->create();
+
+        $this->postJson('/login', [
+
+            'email' => $user->email,
+            'password' => 'wrong-password'
+        ])->assertStatus(422)->assertJson([
+            'message' => 'credentials dont match our records'
+        ]);
+    }
+}
