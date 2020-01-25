7e00cff557bec70dbc912ae0442b98c7f716c426
Davies Maina <daviesmaina@outlook.com>
2020-01-25 18:43:03 +0300

    factory seed for Series

---
+++ database/factories/Series.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Series;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Model;
+
+$factory->define(Series::class, function (Faker $faker) {
+
+    $title = $faker->sentence(5);
+    return [
+
+        'title' => $title,
+        'slug' => str_slug($title),
+        'image_url' => $faker->imageUrl(),
+        'description' => $faker->paragraph()
+    ];
+});
