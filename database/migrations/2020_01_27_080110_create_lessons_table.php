b80dbf19d6febf308e56797ad9d9b243c633672a
Davies Maina <daviesmaina@outlook.com>
2020-01-27 11:20:20 +0300

    lessons table and it's blade template

---
+++ database/migrations/2020_01_27_080110_create_lessons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('series_id');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('episode_number');
+            $table->integer('video_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
---
+++ resources/views/admin/series/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('header')
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+  <h1 class="display-4">{{$series->title}}</h1>
+    <p class="lead">Customize your series lessons</p>
+  </div>
+</div>
+
+@endsection
+    
+
+
+@section('content')
+
+
+@endsection
