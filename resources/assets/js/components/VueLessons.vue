b9ffe977bdba9a8545b7eb34d68550f1dce560ff
Davies Maina <daviesmaina@outlook.com>
2020-01-27 16:20:45 +0300

    create lesson child component modal with form

--- resources/assets/js/components/VueLessons.vue
+++ resources/assets/js/components/VueLessons.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="container">
+        <div class="text-center p-3">
+            <button
+                class="btn btn-primary"
+                @click="createLesson"
+                data-toggle="modal"
+                data-target="#createlessonModal"
+            >
+                Create lesson
+            </button>
+        </div>
+        <br />
+        <hr />
         <ul class="list-group">
             <li
                 class="list-group-item"
@@ -9,16 +21,27 @@
                 {{ lesson.title }}
             </li>
         </ul>
+        <createlesson></createlesson>
     </div>
 </template>
 
 <script>
+import createlesson from "./children/CreateLesson";
 export default {
     props: ["dblessons"],
+    components: {
+        createlesson
+    },
     data() {
         return {
             lessons: this.dblessons
         };
+    },
+
+    methods: {
+        createLesson() {
+            this.$emit("createNewLesson");
+        }
     },
 
     computed: {
---
+++ resources/assets/js/components/children/CreateLesson.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="container">
+        <div
+            class="modal fade"
+            id="createlessonModal"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby=""
+            aria-hidden="true"
+        >
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="">
+                            Create lesson
+                        </h5>
+                        <button
+                            type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <input
+                                type="text"
+                                class="form-control"
+                                placeholder="Lesson title"
+                                v-model="title"
+                            />
+                        </div>
+                        <div class="form-group">
+                            <input
+                                type="number"
+                                class="form-control"
+                                placeholder="video id"
+                                v-model="video_id"
+                            />
+                        </div>
+                        <div class="form-group">
+                            <input
+                                type="number"
+                                class="form-control"
+                                placeholder="Episode no."
+                                v-model="episode_number"
+                            />
+                        </div>
+                        <div class="form-group">
+                            <textarea
+                                cols="30"
+                                rows="10"
+                                class="form-control"
+                                v-model="description"
+                            ></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-secondary"
+                            data-dismiss="modal"
+                        >
+                            Close
+                        </button>
+                        <button type="button" class="btn btn-primary">
+                            Save changes
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    mounted() {
+        this.$parent.$on("createNewLesson", () => {
+            console.log("creatying");
+        });
+    },
+
+    data() {
+        return {
+            title: "",
+            description: "",
+            video_id: "",
+            episode_number: ""
+        };
+    }
+};
+</script>
